c7f3493ff77acbc329ba2325da82790d0a3128c9
Gabriel-Oliveira25 <oliveira.gabriel25@yahoo.com>
2023-09-07 16:38:13 -0300

    first commit

---
+++ emailsender.spring/src/test/java/com/cesarschool/project/emailsender/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesarschool.project.emailsender.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
