59e663036e464c10d1ae9a254840b9bcc27d7291
Gabriel-Oliveira25 <oliveira.gabriel25@yahoo.com>
2023-09-18 19:29:00 -0300

    correção das pastas do projeto

---
+++ src/main/java/com/cesarschool/project/emailsender/spring/controllers/GenericController.java
@@ -0,0 +1,8 @@
+package com.cesarschool.project.emailsender.spring.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GenericController {
+
+}
---
+++ src/main/java/com/cesarschool/project/emailsender/spring/controllers/advice/ControllersAdvice.java
@@ -0,0 +1,9 @@
+package com.cesarschool.project.emailsender.spring.controllers.advice;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+
+@ControllerAdvice
+public class ControllersAdvice {
+	
+}
