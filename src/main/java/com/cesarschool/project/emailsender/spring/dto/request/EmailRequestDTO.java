ad5734b852f5f24cb531b4cd25df64478b4379cc
Gabriel-Oliveira25 <oliveira.gabriel25@yahoo.com>
2023-09-19 00:27:39 -0300

    email sending method filtered by organization completed

--- src/main/java/com/cesarschool/project/emailsender/spring/dto/request/EmailRequestDTO.java
+++ src/main/java/com/cesarschool/project/emailsender/spring/dto/request/EmailRequestDTO.java
@@ -1,13 +1,22 @@
 package com.cesarschool.project.emailsender.spring.dto.request;
 
+import com.cesarschool.project.emailsender.spring.entities.User;
 import com.cesarschool.project.emailsender.spring.enums.StatusMail;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class EmailRequestDTO {
 
+	
+	@NotBlank
+	private User user;
+	
+	@Email
+	@NotBlank
+	private String sendTo;
 
 	@NotBlank
 	private String subject;
--- src/main/java/com/cesarschool/project/emailsender/spring/services/EmailServices.java
+++ src/main/java/com/cesarschool/project/emailsender/spring/services/EmailServices.java
@@ -1,18 +1,23 @@
 package com.cesarschool.project.emailsender.spring.services;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import com.cesarschool.project.emailsender.spring.dto.request.EmailRequestDTO;
 import com.cesarschool.project.emailsender.spring.dto.response.GenericResponseDTO;
 import com.cesarschool.project.emailsender.spring.entities.Email;
 import com.cesarschool.project.emailsender.spring.entities.User;
 import com.cesarschool.project.emailsender.spring.enums.StatusMail;
 import com.cesarschool.project.emailsender.spring.exceptions.GeneralException;
+import com.cesarschool.project.emailsender.spring.repositories.EmailRepository;
 import com.cesarschool.project.emailsender.spring.repositories.MessageRepository;
 import com.cesarschool.project.emailsender.spring.repositories.UserRepository;
 
@@ -28,26 +33,48 @@ public class EmailServices {
 
 	private final JavaMailSender mailSender;
 
+	private final EmailRepository emailRepository;
 
 	public GenericResponseDTO sendMessageByOrganization(String idMessage, String organization) {
 
-		Email e = new Email();
 		Optional.ofNullable(messageRepository.findById(idMessage)).ifPresentOrElse(message -> {
 			Optional.ofNullable(userRepository.findByOrganization(organization)).ifPresentOrElse(user -> {
-
+				
+				EmailRequestDTO e = new EmailRequestDTO();
+				
 				List<User> u = userRepository.findByOrganization(organization);
+				
+				try {
+					SimpleMailMessage email = new SimpleMailMessage();
+					email.setFrom("emailsendernext@gmail.com");
+					email.setTo(u.stream().map(User::getEmail).toArray(String[]::new));
+					email.setSubject(message.get().getSubject());
+					email.setText(message.get().getText());
+					mailSender.send(email);
 
-				SimpleMailMessage email = new SimpleMailMessage();
-				email.setFrom("emailsendernext@gmail.com");
-				email.setTo(u.stream().map(User::getEmail).toArray(String[]::new));
-				email.setSubject(message.get().getSubject());
-				email.setText(message.get().getText());
-				mailSender.send(email);
+					e.setStatusMail(StatusMail.SENT);
+				} catch (MailException ex) {
+					e.setStatusMail(StatusMail.ERROR);
+					throw new GeneralException("ERROR SENDING THE EMAIL", HttpStatus.BAD_GATEWAY);
+				} finally {
+					
+					List<Email> mailSent = new ArrayList<>();
+					
+					for (User users : u) {
+						
+						Email entity = new Email();
+						
+						e.setUser(users);
+						e.setSendTo(users.getEmail());
+						e.setSubject(message.get().getSubject());
+						e.setText(message.get().getText());
+						BeanUtils.copyProperties(e, entity);
+						mailSent.add(entity);
+					}
 
-				e.setStatusMail(StatusMail.SENT);
+					emailRepository.saveAll(mailSent);
 
-				//SALVAR EMAIL NO BANCO DE DADOS
-
+				}
 			}, () -> {
 				throw new GeneralException("USER NOT FOUND IN OUR DATABASE", HttpStatus.NOT_FOUND);
 			});
